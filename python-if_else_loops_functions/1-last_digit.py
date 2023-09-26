03b68c951227c8b58dba75c92aa5c9f4eecd5a5b
JeremMac <macal.jerem@gmail.com>
2023-09-26 10:15:26 +0200

    Fix: condition for zero case.

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,12 +7,12 @@ if (number < 0):
 else:
     last_digit = number % 10
 
-if (last_digit > 5):
-    greater = 'and is greater than 5'
-    print(f"Last digit of {number} is {last_digit} {greater}")
-elif(last_digit < 6):
-    less = 'and is less than 6 and not 0'
-    print(f"Last digit of {number} is {last_digit} {less}")
-else:
+if(last_digit == 0):
     zero = 'and is 0'
     print(f"Last digit of {number} is {last_digit} {zero}")
+elif(last_digit > 5):
+    greater = 'and is greater than 5'
+    print(f"Last digit of {number} is {last_digit} {greater}")
+else:
+    less = 'and is less than 6 and not 0'
+    print(f"Last digit of {number} is {last_digit} {less}")
