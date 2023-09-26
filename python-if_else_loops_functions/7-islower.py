2f9af9f98754623e2dd4e02ec1e41aa34bcbb035
JeremMac <macal.jerem@gmail.com>
2023-09-26 19:03:47 +0200

    Correcting islower code.

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     for i in range(97, 123):
-        if (c == chr(i) and i >= 62):
+        if (c == chr(i) and i >= 97 and i <= 123):
             return (True)
-        elif(c == chr(i) and i <= 123):
-            return(True)
-        else:
+        elif(c == chr(i) and i < 97 and i > 123):
             return(False)
