707ddfde95504d91f3f12034391dbecfb7e5f7e0
JeremMac <macal.jerem@gmail.com>
2023-09-26 10:23:50 +0200

    Changed variable declaration position.

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+zero = 'and is 0'
+greater = 'and is greater than 5'
+less = 'and is less than 6 and not 0'
 
 if (number < 0):
-    last_digit = (number % -10)
+    last_digit = number % -10
 else:
     last_digit = number % 10
 
 if(last_digit == 0):
-    zero = 'and is 0'
     print(f"Last digit of {number} is {last_digit} {zero}")
 elif(last_digit > 5):
-    greater = 'and is greater than 5'
     print(f"Last digit of {number} is {last_digit} {greater}")
 else:
-    less = 'and is less than 6 and not 0'
     print(f"Last digit of {number} is {last_digit} {less}")
