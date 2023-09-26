a0189e34fd0212acb746cfbaead881b4c006169a
JeremMac <macal.jerem@gmail.com>
2023-09-26 19:10:07 +0200

    Fix: space requirement.

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     for i in range(97, 123):
-        if (c == chr(i) and i >= 97 and i <= 123):
+        if (c == chr(i) and i >= 97 and i < 123):
             return (True)
-        elif(c == chr(i) and i < 97 and i > 123):
+        elif (c == chr(i) and i < 97 and i > 123):
             return(False)
