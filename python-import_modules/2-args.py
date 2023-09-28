b863547545b0b040d4a92bddcd4a68e4244eb5f3
JeremMac <macal.jerem@gmail.com>
2023-09-28 15:12:36 +0200

    Added 2-args.py program file.

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv as arg
+i = 1
+length = (len(arg)) - 1
+if (length < 1):
+    print("{} arguments.".format(length))
+elif (length == 1):
+    print("{} argument:".format(length))
+    print(length, arg[1])
+else:
+    print("{} arguments:".format(length))
+    while (i <= length):
+        print(i, ':', arg[i])
+        i += 1
