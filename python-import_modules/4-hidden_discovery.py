08239a337dd9560c878fd3926767229402eb1d65
JeremMac <macal.jerem@gmail.com>
2023-09-28 17:27:31 +0200

    Changing code.

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-for i in hidden_4:
-    if (hidden_4[i] != '_'):
-        print(hidden_4[i])
+    names = dir(hidden_4)
+    sorted_names = sorted(name for name in names if not name.startswith("__"))
+    for name in sorted_names:
+        print(name)
