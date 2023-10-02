844f9d5c33a78bb9125b947816f0cbcaa2086143
JeremMac <macal.jerem@gmail.com>
2023-10-02 15:53:23 +0200

    Adding condition for spaces.

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,7 @@ def print_matrix_integer(matrix=[[]]):
         return (None)
     for i in matrix:
         for num in i:
-            print("{:d}".format(num), end=" ")
+            print("{:d}".format(num), end="")
+            if num != (len(matrix)):
+                print(" ", end="")
         print()
