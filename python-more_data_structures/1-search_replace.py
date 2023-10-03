14651d6fe795dfc824e4bbdd15c3a0178088ce89
JeremMac <macal.jerem@gmail.com>
2023-10-03 16:24:24 +0200

    Fix None case.

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list is not None:
+    if my_list is not None and search is not None and replace is not None:
         new_list = my_list.copy()
         for i in range(len(new_list) - 1):
             if new_list[i] == search:
