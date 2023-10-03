f25357493194a989c42e27d8af7f42c3ed16b515
JeremMac <macal.jerem@gmail.com>
2023-10-03 16:14:38 +0200

    Fix: My list is not None.

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    for i in range(len(new_list) - 1):
-        if new_list[i] == search:
-            new_list[i] = replace
-    return new_list
+    if my_list is not None:
+        new_list = my_list.copy()
+        for i in range(len(new_list) - 1):
+            if new_list[i] == search:
+                new_list[i] = replace
+        return new_list
