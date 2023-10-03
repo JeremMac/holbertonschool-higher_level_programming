448c78379263f84ca34a6dbc174901a2ce4a88a4
JeremMac <macal.jerem@gmail.com>
2023-10-03 17:17:17 +0200

    Fix: len -1 in for loop.

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list is not None and search is not None and replace is not None:
         new_list = my_list.copy()
-        for i in range(len(new_list) - 1):
+        for i in range(len(new_list)):
             if new_list[i] == search:
                 new_list[i] = replace
         return new_list
