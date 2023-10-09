ac157eb1e939e644627f2dd519045749bd75a04c
JeremMac <macal.jerem@gmail.com>
2023-10-09 18:20:40 +0200

    Added position setting to Square class.

---
+++ python-classes/6-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""a module with my class"""
+
+
+class Square:
+    """a class with the size of a square"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    """a function that returns the square value of size"""
+    def area(self):
+        area = self.__size * self.__size
+        return area
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int) is True:
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if isinstance(value, int) is False and len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                if self.__position[0] > 0:
+                    print("_" * self.__position[0], end="")
+                else:
+                    print(" " * self.__position[0], end="")
+                print(self.size * '#')
