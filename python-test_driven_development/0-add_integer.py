5ca7eff58ce67de87c3c2a5aa8a6ba7403cfff73
JeremMac <macal.jerem@gmail.com>
2023-10-10 13:53:45 +0200

    Created repository and Readme, added task zero.

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """a function that adds two integers"""
+
+    if isinstance(a, float) is True:
+        a = int(a)
+    if  isinstance(b, float) is True:
+        b = int(b)
+    if isinstance(a, int) is False:
+        raise TypeError("a must be an integer")
+    if isinstance(b, int) is False:
+        raise TypeError("b must be an integer")
+    return a + b
