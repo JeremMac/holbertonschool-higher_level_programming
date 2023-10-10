84cbd5c5fc763678ec0e1f5ba8bf66f32896d658
JeremMac <macal.jerem@gmail.com>
2023-10-10 16:26:31 +0200

    Added print_square function and its test.

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a module with print_square function definition"""
+
+
+def print_square(size):
+    """a function that print a square"""
+
+    if isinstance(size, int) is False:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) is True and size < 0:
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        print(size *'#')
