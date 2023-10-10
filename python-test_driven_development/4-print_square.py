418d16972525d95d671aeb7644bb64b38a5ffc1e
JeremMac <macal.jerem@gmail.com>
2023-10-10 16:28:34 +0200

    Fix: python coding style.

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -12,4 +12,4 @@ def print_square(size):
     if isinstance(size, float) is True and size < 0:
         raise TypeError("size must be an integer")
     for i in range(size):
-        print(size *'#')
+        print(size * '#')
