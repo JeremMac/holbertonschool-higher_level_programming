fb5b688f3f0cc70dd281b88854b556f10e2c4bad
JeremMac <macal.jerem@gmail.com>
2023-10-11 08:30:34 +0200

    Changed if conditons.

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,8 +5,8 @@
 def say_my_name(first_name, last_name=""):
     """a function to print last name and first name"""
 
-    if isinstance(first_name, str) is False:
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if isinstance(last_name, str) is False:
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,11 +5,9 @@
 def print_square(size):
     """a function that print a square"""
 
-    if isinstance(size, int) is False:
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if isinstance(size, float) is True and size < 0:
-        raise TypeError("size must be an integer")
     for i in range(size):
         print(size * '#')
