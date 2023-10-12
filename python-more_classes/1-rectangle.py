173b52fc6b6adff7b22b838da64f8451d631b305
JeremMac <macal.jerem@gmail.com>
2023-10-12 19:22:29 +0200

    Added property, instances and setter to rectangle.

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""a module that contains the my rectangle class"""
+
+
+class Rectangle:
+    """a class that creates a rectangle
+
+    returns a rectangle
+
+    raises:
+        ___put errors here___
+    """
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
