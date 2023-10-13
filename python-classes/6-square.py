c3e48898da2dafef1ba6bd94c491e9533f00d99f
JeremMac <macal.jerem@gmail.com>
2023-10-13 09:26:57 +0200

    Changed print output.

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -45,5 +45,5 @@ class Square:
         else:
             for i in range(self.__size):
                 if self.__position[0] > 0:
-                    print(" " * self.__position[0], end="")
+                    print("_" * self.__position[0], end="")
                 print(self.__size * '#')
