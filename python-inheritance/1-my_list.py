529c101d7935e82c57b14a18bbe9b6c176b1d2a4
JeremMac <macal.jerem@gmail.com>
2023-10-18 12:11:27 +0200

    Added 1-my_list.py and its tests.

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''A module that contains MyList class'''
+
+
+class MyList(list):
+    '''a class with that inherits a given list.
+    method: print_sorted a function that
+    prints a list in sorted order.
+    '''
+    def print_sorted(self):
+        sorted_list = sorted(self)
+        print(sorted_list)
