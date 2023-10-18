706a6831f3d05876127ec8ba98f26eade366d35f
JeremMac <macal.jerem@gmail.com>
2023-10-18 11:47:01 +0200

    Added documentation.

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+'''A module that define the lookup function'''
 def lookup(obj):
+    '''A function that returns a list of instances
+    in an object.
+    '''
     return dir(obj)
