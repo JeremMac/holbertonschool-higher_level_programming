c9cf5729ec79bda49980877d0df983ba90cef578
JeremMac <macal.jerem@gmail.com>
2023-10-19 12:44:21 +0200

    Added 10-square.py file.

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''A module that contains a square class'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(BaseGeometry):
+    '''a class that creates a square'''
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
