a76bd32fa2af98caa90ce35d271888064d531005
JeremMac <macal.jerem@gmail.com>
2023-10-19 16:33:00 +0200

    Added 3-to_json_string.py file.

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''A module that defines the from_json_string function'''
+import json
+
+
+def from_json_string(my_str):
+    '''A function that returns an object
+    represented by a JSON string.
+
+    args:
+        my_str: the string we need to convert.
+
+    returns: an object from my_str.
+    '''
+
+    return json.load(my_str)
