7918990fe2b2ab34023e7d731d3dcc72a3c2f175
JeremMac <macal.jerem@gmail.com>
2023-10-24 13:31:41 +0200

    Fix: to_json_string condition.

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -28,6 +28,7 @@ class Base:
         '''A method that that returns the JSON
         string representation of a dictionary'''
 
-        if list_dictionaries is None:
+        if list_dictionaries is None or\
+                len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
