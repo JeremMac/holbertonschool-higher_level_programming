7efa3c231a80d26d914fbd5bf635d9c78c032d48
JeremMac <macal.jerem@gmail.com>
2023-10-24 11:27:24 +0200

    Fix: missing documentation for Square.

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,6 +4,14 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    '''
+    A class that creates a square.
+
+    instances:
+        size: the int value of the width and height of Square.
+        x: axe position for Square.
+        y: second axe Position for square.
+    '''
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
