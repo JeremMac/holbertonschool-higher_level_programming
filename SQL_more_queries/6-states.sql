573539bb72500538ff5022daa2e2c1235955d9ae
JeremMac <macal.jerem@gmail.com>
2023-11-22 11:25:53 +0100

    Added 6-states.sql file.

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE if NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    UNIQUE KEY (id),
+    PRIMARY KEY (id)
+);
