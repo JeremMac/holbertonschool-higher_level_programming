87f527778e4a08dd52cd69ab4c26ad7627b934dd
JeremMac <macal.jerem@gmail.com>
2023-11-24 10:22:53 +0100

    Fix: module documentation.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
-"""A module that contains a script that lists
-all states from the database hbtn_0e_0_usa."""
+"""
+A module that contains a script that lists
+all states from the database hbtn_0e_0_usa.
+"""
 
 import MySQLdb
 import sys
