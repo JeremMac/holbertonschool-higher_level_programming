ef25d820a69c52398ed1027922d2d978867fbfbb
JeremMac <macal.jerem@gmail.com>
2023-11-24 15:09:12 +0100

    Added 3-my_safe_filter_states.py file.

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module that list all states from a database.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        exit(1)
+
+    database = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3],
+    )
+
+    cursor = database.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY %s\
+                    ORDER BY states.id", (sys.argv[4],))
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    database.close()
