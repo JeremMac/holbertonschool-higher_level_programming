7534bc68cc6c5e2a695ecc60a64f118382a6878e
JeremMac <macal.jerem@gmail.com>
2023-11-24 10:11:32 +0100

    Added print.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,6 +9,7 @@ import sys
 if __name__ == "__main__":
 
     if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
 
     database = MySQLdb.connect(
