f60883e35a212e0ef33a97505affd60e6e7517d2
JeremMac <macal.jerem@gmail.com>
2023-11-24 09:54:31 +0100

    Fix: import command.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,25 +5,25 @@ A module that contains a script that lists
 all states from the database hbtn_0e_0_usa.
 '''
 
+import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
 
-if len(argv) != 4:
-    print("error")
-    exit(1)
+    if len(argv) != 4:
+        print("error")
+        exit(1)
 
-conn = MySQLdb.connect(
-    host="localhost",
-    user=argv[1],
-    passwd=argv[2],
-    database=argv[3],
-    port=3306
-)
+    conn = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        database=argv[3],
+        port=3306
+    )
 
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM states ORDER BY states.id")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id")
 
 for i in cursor.fetchall():
     print(i)
