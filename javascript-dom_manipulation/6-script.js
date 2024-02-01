9e2cb69cb07c11fa8a1310f3a8e671adc3d6adfc
JeremMac <macal.jerem@gmail.com>
2024-02-01 11:59:18 +0100

    Added 6-script.js and its html file.

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+    .then(response => response.json())
+    .then(data => {
+      const characterName = data.name;
+      document.getElementById('character').textContent = characterName;
+    })
+    .catch(error => {
+      console.log('Error fetching character name:', error);
+    });
+});
