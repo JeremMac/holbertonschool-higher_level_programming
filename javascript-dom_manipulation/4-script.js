3b37e70969d6c64cb91531507c79bd50f03bed91
JeremMac <macal.jerem@gmail.com>
2024-02-01 10:51:39 +0100

    Added 4-script.js and its html file.

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const item = document.querySelector('#add_item');
+    const MyList = document.querySelector('.my_list');
+    item.addEventListener('click', function () {
+        const NewItem = document.createElement('li');
+        NewItem.textContent = 'Item';
+        MyList.appendChild(NewItem);
+    });
+});
