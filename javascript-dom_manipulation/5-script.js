087b0fb1580d554c57d895d6adcd243c10aed483
JeremMac <macal.jerem@gmail.com>
2024-02-01 11:11:09 +0100

    Added 5-script.js and it html file.

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('header');
+  const update = document.querySelector('#update_header');
+  update.addEventListener('click', function () {
+    header.textContent = 'New Header!!!';
+  });
+});
